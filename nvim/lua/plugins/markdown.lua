c3857cd2918364b2660b2368efc93aa9ef851f7c
radouane-tamouss <atamousse.red@gmail.com>
2024-12-31 19:16:49 +0100

    add markdown support for neovim!

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  config = function()
+    local render_markdown = require("render-markdown")
+    render_markdown.setup({
+      -- Your configuration settings here
+    })
+  end,
+}
--- nvim/lua/plugins/norminette.lua
+++ nvim/lua/plugins/norminette.lua
@@ -1,11 +1,12 @@
 -- ~/.config/nvim/lua/plugins/norminette.lua
+
 return {
   "hardyrafael17/norminette42.nvim",
   config = function()
     local norminette = require("norminette")
     norminette.setup({
       runOnSave = true, -- Optional, check for errors after save
-      -- maxErrorsToShow = 5, -- Only show 5 errors
+      maxErrorsToShow = 5, -- Only show 5 errors
       active = true, -- Optional, can be set to false to deactivate plugin
     })
   end,
