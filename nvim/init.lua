d209755b7f3eb0a4e477a81c2c96d3ed563b123a
TAMOUSS Radouane <atamousse.red@gmail.com>
2025-03-15 12:42:22 +0000

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+})
